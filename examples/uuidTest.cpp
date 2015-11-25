e4fb13a086320508a44597bb319916494caec720
bjtj <bjtj10@gmail.com>
2015-11-25 17:41:03 +0900

    win32 project updated

--- examples/uuidTest.cpp
+++ examples/uuidTest.cpp
@@ -6,11 +6,15 @@ using namespace UPNP;
 
 int main(int argc, char * args[]) {
     
-    string sample = "12345678-1234-abcd-ef12-1122aabbccdd";
+    //string sample = "12345678-1234-abcd-ef12-1122aabbccdd";
+	string sample = "2fac1234-31f8-11b4-a222-08002b34c003";
+	
     
     Uuid::validFormat(sample);
     
     cout << "valid" << endl;
+
+	getchar();
     
     return 0;
 }
