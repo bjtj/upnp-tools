d85afd4e2d70001d923d4772217bd3619cdc82d6
bjtj <bjtj10@gmail.com>
2016-03-16 15:16:05 +0900

    refactoring

--- libupnp-tools/UPnPActionInvoker.hpp
+++ libupnp-tools/UPnPActionInvoker.hpp
@@ -16,20 +16,21 @@ namespace UPNP {
 	class UPnPActionInvoker {
 	private:
 		HTTP::Url _baseUrl;
-		UTIL::AutoRef<UPnPService> service;
+		std::string serviceType;
+		std::string controlUrl;
 		UPnPAction _action;
 		std::map<std::string, std::string> _inParams;
 		std::map<std::string, std::string> _outParams;
 		
 	public:
-		UPnPActionInvoker(HTTP::Url baseUrl, UTIL::AutoRef<UPnPService> service, UPnPAction action) : _baseUrl(baseUrl), service(service), _action(action) {}
+		UPnPActionInvoker(HTTP::Url baseUrl, UTIL::AutoRef<UPnPService> service, UPnPAction action) : _baseUrl(baseUrl), _action(action) {
+			serviceType = service->getServiceType();
+			controlUrl = service->getControlUrl();
+		}
 		virtual ~UPnPActionInvoker() {}
 
 		HTTP::Url & baseUrl() {
 			return _baseUrl;
-		}
-		UTIL::AutoRef<UPnPService> getService() {
-			return service;
 		}
 		UPnPAction action() {
 			return _action;
@@ -42,8 +43,8 @@ namespace UPNP {
 		}
 		void invoke() {
 			UTIL::LinkedStringMap headers;
-			headers["SOAPACTION"] = ("\"" + service->getServiceType() + "#" + _action.name() + "\"");
-			HTTP::Url url = _baseUrl.relativePath(service->getControlUrl());
+			headers["SOAPACTION"] = ("\"" + serviceType + "#" + _action.name() + "\"");
+			HTTP::Url url = _baseUrl.relativePath(controlUrl);
 			std::string result = HttpUtils::httpPost(url, headers, makeSoapRequestContent());
 			XML::XmlDocument doc = XML::DomParser::parse(result);
 			if (doc.getRootNode().nil()) {
@@ -65,7 +66,7 @@ namespace UPNP {
 			request = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\r\n";
 			request.append("<s:Envelope s:encodingStyle=\"http://schemas.xmlsoap.org/soap/encoding/\" xmlns:s=\"http://schemas.xmlsoap.org/soap/envelope/\">\r\n");
 			request.append("<s:Body>\r\n");
-			request.append("<u:" + _action.name() + " xmlns:u=\"" + service->getServiceType() + "\">\r\n");
+			request.append("<u:" + _action.name() + " xmlns:u=\"" + serviceType + "\">\r\n");
 			for (std::map<std::string, std::string>::iterator iter = _inParams.begin(); iter != _inParams.end(); iter++) {
 				std::string name = iter->first;
 				std::string & value = iter->second;
