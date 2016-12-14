8fb0a553428538232849a5cdaa29f35e47a1748c
bjtj <bjtj10@gmail.com>
2016-12-14 11:17:59 +0000

    ssdp discover updated

--- examples/ssdp-discover.cpp
+++ examples/ssdp-discover.cpp
@@ -7,21 +7,42 @@ using namespace OS;
 using namespace UTIL;
 using namespace SSDP;
 
-class MySSDPEventListener : public SSDPEventListener {
+class Debug {
+private:
+	bool _verbose;
 public:
-	MySSDPEventListener() {}
+    Debug() : _verbose(false) {}
+    virtual ~Debug() {}
+	bool & verbose() {return _verbose;}
+};
+
+
+class MySSDPEventListener : public SSDPEventListener {
+private:
+	Debug & debug;
+public:
+	MySSDPEventListener(Debug & debug) : debug(debug) {}
 	virtual ~MySSDPEventListener() {}
 
 	virtual void onMsearch(SSDPHeader & header) {
 		cout << "M-SEARCH : " << header["ST"] << endl;
+		if (debug.verbose()) {
+			cout << " ** " << header.toString() << endl;
+		}
 	}
 
 	virtual void onNotify(SSDPHeader & header) {
 		cout << "NOTIFY : " << header["NTS"] << " - " << header["LOCATION"] << endl;
+		if (debug.verbose()) {
+			cout << " ** " << header.toString() << endl;
+		}
 	}
 
 	virtual void onMsearchResponse(SSDPHeader & header) {
 		cout << "RESP : " << header["LOCATION"] << endl;
+		if (debug.verbose()) {
+			cout << " ** " << header.toString() << endl;
+		}
 	}
 };
 
@@ -32,21 +53,24 @@ int main(int argc, char *args[]) {
 
 	bool done = false;
 	SSDPServer server;
+	Debug debug;
 
 	server.startAsync();
 	server.supportMsearchAsync(true);
 
-	server.addSSDPEventListener(AutoRef<SSDPEventListener>(new MySSDPEventListener));
+	server.addSSDPEventListener(AutoRef<SSDPEventListener>(new MySSDPEventListener(debug)));
 
 	while (!done) {
 		FileStream fs(stdin);
-		string line;
-		if ((line = fs.readline()) == "q") {
+		string line = fs.readline();
+		if (line == "q") {
 			done = true;
 			break;
+		} else if (line == "v") {
+			debug.verbose() = !debug.verbose();
+		} else {
+			server.sendMsearchAsync(line, 3);
 		}
-
-		server.sendMsearchAsync(line, 3);
 	}
 
 	server.stop();
