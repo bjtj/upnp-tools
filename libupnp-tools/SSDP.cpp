c4aee75d7c14b3b015f497e50074fe6b7a7e3a27
bjtj <bjtj10@gmail.com>
2018-09-01 15:57:33 +0900

    fixed ssdp host format

--- libupnp-tools/SSDP.cpp
+++ libupnp-tools/SSDP.cpp
@@ -10,9 +10,8 @@ namespace upnp {
 	string SSDP::GROUP = "239.255.255.250";
 	int SSDP::PORT = 1900;
 
-
 	string SSDP::host() {
-		return GROUP + Text::toString(PORT);
+		return GROUP + ":" + Text::toString(PORT);
 	}
 	
 }
